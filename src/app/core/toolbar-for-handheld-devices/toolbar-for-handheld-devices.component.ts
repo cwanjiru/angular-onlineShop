6a8f55fde8374f1a09b3a43c0d388a851a4efa10
christine <gacuirichristine@gmail.com>
2020-08-06 22:07:19 +0300

    Generate ToolbarForHandhelDevicesComponent.

---
+++ src/app/core/toolbar-for-handheld-devices/toolbar-for-handheld-devices.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ots-toolbar-for-handheld-devices',
+  templateUrl: './toolbar-for-handheld-devices.component.html',
+  styles: [
+  ]
+})
+export class ToolbarForHandheldDevicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
