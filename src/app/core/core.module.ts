aa54eff3b53fb57e17ff0e69cd9b15ad28edd097
christine <gacuirichristine@gmail.com>
2020-08-06 22:05:40 +0300

    Register ToolbarForHandheldDevicesComponent.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ToolbarForHandheldDevicesComponent } from './toolbar-for-handheld-devices/toolbar-for-handheld-devices.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, ToolbarForHandheldDevicesComponent],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
+    ToolbarForHandheldDevicesComponent,
   ]
 })
 export class CoreModule { }
