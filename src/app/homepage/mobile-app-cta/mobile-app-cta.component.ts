b3408f0a572f4233eecfd2b94630abb739ab9ca5
christine <gacuirichristine@gmail.com>
2020-08-13 23:36:13 +0300

    Generate MobileCtaComponent.

---
+++ src/app/homepage/mobile-app-cta/mobile-app-cta.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ots-mobile-app-cta',
+  templateUrl: './mobile-app-cta.component.html',
+  styles: [
+  ]
+})
+export class MobileAppCtaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
