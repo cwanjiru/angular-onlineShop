46743214009144fd3e0fd0f19cbe474ee7b6138f
christine <gacuirichristine@gmail.com>
2020-08-13 23:34:19 +0300

    Register MobileAppCtaComponent.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RouterModule } from '@angular/router';
+import { MobileAppCtaComponent } from './mobile-app-cta/mobile-app-cta.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, MobileAppCtaComponent],
   imports: [
     CommonModule,
     RouterModule,
