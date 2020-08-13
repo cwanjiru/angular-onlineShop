20932f796ca21ede533eefdb3a684d769e414b04
christine <gacuirichristine@gmail.com>
2020-08-13 20:30:27 +0300

    Add a mock datasource that takes in data from the interface.

---
+++ src/app/core/mock-food-categories.ts
@@ -0,0 +1,11 @@
+import { FoodCategory } from './food-category';
+
+export const FOOD_CATEGORIES: Array<FoodCategory>=[
+    {slug:'burgers-and-fries', title:'Burgers & Fries', image:'/assets/img/food-delivery/category/01.jpg'},
+    { slug: 'noodles', title: 'Noodles', image: '/assets/img/food-delivery/category/02.jpg' },
+    { slug: 'sushi-and-rolls', title: 'Sushi & Rolls', image: '/assets/img/food-delivery/category/03.jpg' },
+    { slug: 'pizza-and-pasta', title: 'Pizza & Pasta', image: '/assets/img/food-delivery/category/04.jpg' },
+    { slug: 'coffee-and-desserts', title: 'Coffee & Desserts', image: '/assets/img/food-delivery/category/05.jpg' },
+    { slug: 'healthy-and-food', title: 'Healthy & Food', image: '/assets/img/food-delivery/category/06.jpg' }
+]
+
